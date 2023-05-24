6cfa6743a8e00eaba96d400900cc3f643d323e84
Nguyen Hoai Phong <andyhoaiphong2003@gmail.com>
2023-05-24 23:29:31 +0700

    re-config env

--- src/global/firebase.ts
+++ src/global/firebase.ts
@@ -9,13 +9,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBlvd7AQEwVJi2HzWbr-9ob2UyuxTFMAYw',
-  authDomain: 'document-management-syst-7b9cd.firebaseapp.com',
-  projectId: 'document-management-syst-7b9cd',
-  storageBucket: 'document-management-syst-7b9cd.appspot.com',
-  messagingSenderId: '386688300529',
-  appId: '1:386688300529:web:aa4fe3ac7cf702fdbe4050',
-  measurementId: 'G-97BN4WRPHW'
+  apiKey: import.meta.env.VITE_API_KEY as string,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN as string,
+  projectId: import.meta.env.VITE_PROJECT_ID as string,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET as string,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID as string,
+  appId: import.meta.env.VITE_APP_ID as string,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID as string
 }
 
 // Initialize Firebase

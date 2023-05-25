0284e45d5ccfff4516da510605f24eb9f003af73
Nguyen Hoai Phong <andyhoaiphong2003@gmail.com>
2023-05-25 19:45:48 +0700

    fix casting data type

--- src/routers/home/Welcome.tsx
+++ src/routers/home/Welcome.tsx
@@ -2,11 +2,12 @@ import { Button, Card } from '@mui/material'
 import { UserAuth } from '~/context/AuthContext'
 const Welcome = () => {
   const { user, logout } = UserAuth()
+
   return (
     <div>
       <Card sx={{ padding: '2rem', width: 'fit-content' }}>
         <h1>Welcome {user?.displayName}</h1>
-        <img src={user?.photoURL} alt='' srcSet='' />
+        <img src={String(user?.photoURL)} alt='' srcSet='' />
       </Card>
       <Button onClick={logout} variant='outlined'>
         Logout

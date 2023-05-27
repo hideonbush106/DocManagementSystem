dfd6278970e44699bc90628a1ce60e4368ce7a4d
Nguyen Hoai Phong <87291170+hideonbush106@users.noreply.github.com>
2023-05-27 22:49:22 +0700

    Revert "Feature/DMS-14: Sign in Login"

--- src/hooks/useAuth.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { AuthContext, AuthContextType } from '~/context/AuthContext'
-
-const useAuth = () => {
-  return React.useContext(AuthContext) as AuthContextType
-}
-
-export default useAuth

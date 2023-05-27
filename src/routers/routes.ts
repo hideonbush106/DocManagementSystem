6317e903ccbc71793aee39906350e3bde9cdae14
Nguyen Hoai Phong <87291170+hideonbush106@users.noreply.github.com>
2023-05-27 22:49:22 +0700

    Revert "Feature/DMS-14: Sign in Login"

--- src/routers/routes.ts
+++
@@ -1,6 +0,0 @@
-import Login from '~/pages/auth/Login'
-import Welcome from '~/pages/home/Welcome'
-
-export const publicRoutes = [{ path: '/', component: Login }]
-
-export const privateRoutes = [{ path: '/welcome', component: Welcome }]

1b39c6d242cb4555abbdf4358ac6c8646f006f1b
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2023-05-27 11:15:10 +0700

    Update AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -20,10 +20,6 @@ const AuthProvider = ({ children }: Props) => {
   const login = async () => {
     try {
       const result: UserCredential = await signInWithPopup(auth, provider)
-      // const credential = GoogleAuthProvider.credentialFromResult(result)
-      // if (credential === null) {
-      //   throw new Error('credential is null')
-      // }
       result.user?.getIdToken().then((token) => localStorage.setItem('userAccessToken', JSON.stringify(token)))
       window.location.href = '/welcome'
     } catch (error) {
@@ -43,18 +39,12 @@ const AuthProvider = ({ children }: Props) => {
   }
 
   useEffect(() => {
-    // const date = Date.now() / 1000
     const unsubscribe = auth.onAuthStateChanged((user) => {
       console.log(
         'user',
         user?.getIdToken().then((token) => console.log(token))
       )
       setUser(user)
-      // user?.getIdTokenResult().then((result) => {
-      //   if (!(Date.parse(result.expirationTime) < date)) {
-      //     user?.getIdTokenResult(true)
-      //   }
-      // })
       return () => unsubscribe()
     })
   }, [user])

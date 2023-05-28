addd937925745d1cd9a4fc5a4d0423488fca1089
DaiNghia0212 <dainghia12311@gmail.com>
2023-05-28 14:51:32 +0700

    restore lost files

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  },
-  css: {
-    devSourcemap: true
-  },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './src')
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: env.APP_ENV
+    },
+    plugins: [react()],
+    server: {
+      port: 3000
+    },
+    css: {
+      devSourcemap: true
+    },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src')
+      }
     }
   }
 })

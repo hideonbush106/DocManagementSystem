980cbf9a4275a84388bb823a58d4fdf92e4bb89a
ddwcsanh <ledoducanh03@gmail.com>
2023-05-30 11:43:14 +0700

    update sidebar avatar and button

--- src/components/sidebar/Sidebar.styled.ts
+++ src/components/sidebar/Sidebar.styled.ts
@@ -4,3 +4,42 @@ export const Wrapper = styled.section`
   width: 15vw;
   height: 100vh;
 `
+
+export const Avatar = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 2rem 0;
+
+`
+
+export const Heading = styled.text`
+  font-size: 1.2rem;
+  font-weight: 600;
+  margin: 10px;
+`
+
+export const Image = styled.div`
+  margin: 2rem 0 1rem;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: gray;
+  overflow: hidden;
+`
+
+export const Menu = styled.div`
+  margin: 1.5rem 0;
+  padding-left: 2rem;
+`
+export const Option = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 2rem 0;
+`
+export const Text = styled.text`
+  margin-left: 1rem;
+  color: var(--grey-color);
+  font-size: 14px;
+`

f6974f63b15a66a8f5ec725148df7ea612c6c369
ddwcsanh <ledoducanh03@gmail.com>
2023-06-09 21:04:15 +0700

    Responsive Sidebar #2

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,23 +1,20 @@
-import { Avatar, Image, LogOut, Menu, Option, Role, Wrapper, LinkContainer } from './Sidebar.styled'
-import { Icon, Typography, styled } from '@mui/material'
-import LogoutIcon from '@mui/icons-material/Logout'
-import { Options } from './OptionsStaff'
 import React, { useEffect, useState } from 'react'
+import { Avatar, Image, LogOut, Menu, Option, Wrapper, LinkContainer, SideBarWrapper } from './Sidebar.styled'
+import { Icon, Typography } from '@mui/material'
+import { Options } from './OptionsStaff'
 import useAuth from '~/hooks/useAuth'
 import { useLocation } from 'react-router-dom'
-
-const Text = styled(Typography)({
-  lineHeight: '1.5rem',
-  fontSize: '14px',
-  paddingLeft: '0.5rem'
-})
+import Drawer from '@mui/material/Drawer'
+import Button from '@mui/material/Button'
+import MenuIcon from '@mui/icons-material/Menu'
+import LogoutIcon from '@mui/icons-material/Logout'
 
 const Sidebar = () => {
   const { user, logout } = useAuth()
-  const [btn, setButton] = useState<number | null>(1)
+  const [btn, setButton] = useState<number | null>(1) //dashboard is default option
 
+  //set option bold when navigate to its address
   const location = useLocation()
-
   useEffect(() => {
     const option = Options.find((option) => location.pathname.includes(`/${option.link}`))
     if (option) {
@@ -27,16 +24,74 @@ const Sidebar = () => {
     }
   }, [location])
 
-  return (
-    <Wrapper>
+  //set togle on or off in mobile view, default off
+  const [toggle, setToggle] = React.useState(false)
+
+  //onclick event for mobile view
+  const toggleDrawer = (open: boolean) => (event: React.MouseEvent) => {
+    if (event.type === 'keydown') {
+      return
+    }
+    setToggle(open)
+  }
+
+  //menu for mobile view
+  const menu = () => (
+    <Menu onClick={toggleDrawer(false)}>
       <Avatar>
         <Image src={String(user?.photoURL)} alt='Your Avatar' />
-        <Typography align='center' sx={{ width: '100%', fontWeight: 600 }}>
+        <Typography align='center' sx={{ width: '100%', fontWeight: 600 }} whiteSpace={'nowrap'}>
           {user?.displayName}
         </Typography>
-        <Role>Staff</Role>
+        <Typography color={'var(--gray-color)'}>Staff</Typography>
       </Avatar>
-      <Menu>
+      {Options.map((option) => (
+        <Option key={option.id}>
+          <LinkContainer to={`/${option.link}`} onClick={() => setButton(option.id)}>
+            <Icon
+              sx={{
+                fontWeight: option.id === btn ? 'bold' : 'normal',
+                color: option.id === btn ? 'var(--black-color)' : 'var(--gray-color)'
+              }}
+            >
+              {React.createElement(option.icon)}
+            </Icon>
+            <Typography
+              sx={{
+                fontWeight: option.id === btn ? 'bold' : 'normal',
+                color: option.id === btn ? 'var(--black-color)' : 'var(--gray-color)',
+                paddingLeft: '0.5rem'
+              }}
+            >
+              {option.text}
+            </Typography>
+          </LinkContainer>
+        </Option>
+      ))}
+      <LogOut onClick={logout}>
+        <LogoutIcon sx={{ color: 'var(--red-color)' }} />
+        <Typography sx={{ color: 'var(--red-color)', fontWeight: 600, paddingLeft: '0.5rem' }}>Log Out</Typography>
+      </LogOut>
+    </Menu>
+  )
+  return (
+    <Wrapper>
+      <SideBarWrapper mobile>
+        <Button onClick={toggleDrawer(true)}>
+          <MenuIcon />
+        </Button>
+        <Drawer anchor={'left'} open={toggle} onClose={toggleDrawer(false)}>
+          {menu()}
+        </Drawer>
+      </SideBarWrapper>
+      <SideBarWrapper desktop>
+        <Avatar>
+          <Image src={String(user?.photoURL)} alt='Your Avatar' />
+          <Typography align='center' sx={{ width: '100%', fontWeight: 600 }}>
+            {user?.displayName}
+          </Typography>
+          <Typography color={'var(--gray-color)'}>Staff</Typography>
+        </Avatar>
         {Options.map((option) => (
           <Option key={option.id}>
             <LinkContainer to={`/${option.link}`} onClick={() => setButton(option.id)}>
@@ -48,22 +103,23 @@ const Sidebar = () => {
               >
                 {React.createElement(option.icon)}
               </Icon>
-              <Text
+              <Typography
                 sx={{
                   fontWeight: option.id === btn ? 'bold' : 'normal',
-                  color: option.id === btn ? 'var(--black-color)' : 'var(--gray-color)'
+                  color: option.id === btn ? 'var(--black-color)' : 'var(--gray-color)',
+                  paddingLeft: '0.5rem'
                 }}
               >
                 {option.text}
-              </Text>
+              </Typography>
             </LinkContainer>
           </Option>
         ))}
-      </Menu>
-      <LogOut onClick={logout}>
-        <LogoutIcon sx={{ color: 'var(--red-color)' }} />
-        <Typography sx={{ color: 'var(--red-color)', fontWeight: 600, paddingLeft: '0.5rem' }}>Log Out</Typography>
-      </LogOut>
+        <LogOut onClick={logout}>
+          <LogoutIcon sx={{ color: 'var(--red-color)' }} />
+          <Typography sx={{ color: 'var(--red-color)', fontWeight: 600, paddingLeft: '0.5rem' }}>Log Out</Typography>
+        </LogOut>
+      </SideBarWrapper>
     </Wrapper>
   )
 }

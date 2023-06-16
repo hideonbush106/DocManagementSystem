25fa982d87712e674afb8a2dfa155173122f748d
DaiNghia0212 <dainghia12311@gmail.com>
2023-06-16 09:13:23 +0700

    config API caller

---
+++ src/hooks/api/useUserApi.tsx
@@ -0,0 +1,30 @@
+import useApi from './useApi'
+
+const useUserApi = () => {
+  const callApi = useApi()
+
+  const getUserLogin = async (token: string) => {
+    const endpoint = '/user/login'
+    const headers = { Authentication: token }
+    try {
+      const response = await callApi('get', endpoint, headers)
+      return response
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getUserOwn = async () => {
+    const endpoint = '/user/login'
+    try {
+      const response = await callApi('get', endpoint)
+      return response
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return { getUserLogin, getUserOwn }
+}
+
+export default useUserApi

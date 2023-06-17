02bd706519e8521dfe2370a1ac52dcf692dac4fd
DaiNghia0212 <dainghia12311@gmail.com>
2023-06-17 11:02:37 +0700

    get file documents in folder

--- src/pages/document/file/File.tsx
+++ src/pages/document/file/File.tsx
@@ -1,16 +1,28 @@
+import React from 'react'
 import { Breadcrumbs } from '@mui/material'
 import { Link, useParams } from 'react-router-dom'
 import DocumentCardList from '~/components/card/DocumentCardList'
+import useDocumentApi from '~/hooks/api/useDocumentApi'
 import useData from '~/hooks/useData'
+import { File as FileType } from '~/global/interface'
 
 const File = () => {
+  const [files, setFile] = React.useState<FileType[]>([])
   const { departmentId, roomId, lockerId, folderId } = useParams()
   const { documentMap } = useData()
+  const { getDocumentsInFolder } = useDocumentApi()
   const department = documentMap.get(departmentId as string)
   const room = department?.roomMap?.get(roomId as string)
   const locker = room?.lockerMap?.get(lockerId as string)
   const folder = locker?.folderMap?.get(folderId as string)
-  const files = folder?.files?.map((file) => ({ id: file.id, name: file.name }))
+
+  React.useEffect(() => {
+    if (folder) {
+      getDocumentsInFolder(folder.id).then(({ data }) => {
+        setFile(data as FileType[])
+      })
+    }
+  }, [folder, getDocumentsInFolder])
 
   return (
     <>
@@ -21,7 +33,7 @@ const File = () => {
         <Link to={`${location.pathname.substring(0, location.pathname.indexOf('folder'))}`}>{locker?.name}</Link>
         <p>{folder?.name}</p>
       </Breadcrumbs>
-      <DocumentCardList type='locker' items={files ? files : []} />
+      <DocumentCardList type='file' items={files ? files : []} />
     </>
   )
 }

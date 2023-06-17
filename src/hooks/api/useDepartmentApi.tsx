44d767ee0aac423c48a13143d8d51311933e3d31
DaiNghia0212 <dainghia12311@gmail.com>
2023-06-17 11:19:14 +0700

    fix unnecessary re-render

--- src/hooks/api/useDepartmentApi.tsx
+++ src/hooks/api/useDepartmentApi.tsx
@@ -1,31 +1,38 @@
 import { CreateDepartment, UpdateDepartment } from '~/global/interface'
 import useApi from './useApi'
+import React from 'react'
 
 const useDepartmentApi = () => {
   const rootEndpoint = 'departments'
   const callApi = useApi()
 
-  const getDepartment = async (department: string) => {
-    const endpoint = `/${rootEndpoint}/${department}`
-    try {
-      const response = await callApi('get', endpoint)
-      return response
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  const getDepartment = React.useCallback(
+    async (department: string) => {
+      const endpoint = `/${rootEndpoint}/${department}`
+      try {
+        const response = await callApi('get', endpoint)
+        return response
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    [callApi]
+  )
 
-  const deleteDepartment = async (departmentId: string) => {
-    const endpoint = `/${rootEndpoint}/${departmentId}`
-    try {
-      const response = await callApi('delete', endpoint)
-      return response
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  const deleteDepartment = React.useCallback(
+    async (departmentId: string) => {
+      const endpoint = `/${rootEndpoint}/${departmentId}`
+      try {
+        const response = await callApi('delete', endpoint)
+        return response
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    [callApi]
+  )
 
-  const getAllDepartments = async () => {
+  const getAllDepartments = React.useCallback(async () => {
     const endpoint = `/${rootEndpoint}`
     try {
       const response = await callApi('get', endpoint)
@@ -33,27 +40,33 @@ const useDepartmentApi = () => {
     } catch (error) {
       console.log(error)
     }
-  }
+  }, [callApi])
 
-  const createDepartment = async (data: CreateDepartment) => {
-    const endpoint = `/${rootEndpoint}`
-    try {
-      const response = await callApi('post', endpoint, {}, {}, data)
-      return response
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  const createDepartment = React.useCallback(
+    async (data: CreateDepartment) => {
+      const endpoint = `/${rootEndpoint}`
+      try {
+        const response = await callApi('post', endpoint, {}, {}, data)
+        return response
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    [callApi]
+  )
 
-  const updateDepartment = async (data: UpdateDepartment) => {
-    const endpoint = `/${rootEndpoint}`
-    try {
-      const response = await callApi('put', endpoint, {}, {}, data)
-      return response
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  const updateDepartment = React.useCallback(
+    async (data: UpdateDepartment) => {
+      const endpoint = `/${rootEndpoint}`
+      try {
+        const response = await callApi('put', endpoint, {}, {}, data)
+        return response
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    [callApi]
+  )
 
   return { getDepartment, deleteDepartment, getAllDepartments, createDepartment, updateDepartment }
 }

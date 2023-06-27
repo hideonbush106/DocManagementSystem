ece03da32aa109b00e8e421d3e5ff7a57e5a6f3f
Nguyen Hoai Phong <andyhoaiphong2003@gmail.com>
2023-06-27 07:35:41 +0700

    add PDF modal

---
+++ src/pages/home/Test.styled.ts
@@ -0,0 +1,7 @@
+import { Viewer, Worker } from '@react-pdf-viewer/core'
+import styled from 'styled-components'
+
+export const PdfContainer = styled(Viewer)`
+  width: 50%;
+  border: 5px solid #000;
+`
--- src/pages/home/Test.tsx
+++ src/pages/home/Test.tsx
@@ -1,15 +1,8 @@
-import { Viewer, Worker } from '@react-pdf-viewer/core'
 import '@react-pdf-viewer/core/lib/styles/index.css'
+import PdfView from '~/components/modal/pdfViewer/PdfView'
 
 const Test = () => {
-  const [, setOpen] = useState(false)
-
-  const handleClose = () => {
-    setOpen(false)
-  }
-  return (
-    <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js'>
-      <Viewer fileUrl='assets/progit.pdf' />
-    </Worker>
-  )
+  return <PdfView />
 }
+
+export default Test

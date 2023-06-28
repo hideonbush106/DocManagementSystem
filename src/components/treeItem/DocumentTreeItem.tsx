11f2eaf8682ea0d08ec2f5c8121d7372daec8301
ddwcsanh <ledoducanh03@gmail.com>
2023-06-28 10:46:57 +0700

    update style font family

--- src/components/treeItem/DocumentTreeItem.tsx
+++ src/components/treeItem/DocumentTreeItem.tsx
@@ -24,11 +24,15 @@ const DocumentTreeItem = (props: Props) => {
       label={
         !loading ? (
           <Box sx={{ display: 'flex', alignItems: 'center', p: 0.5, pr: 0 }}>
-            <Box component={LabelIcon} color='inherit' sx={{ mr: 1 }} />
-            <Typography variant='body2' sx={{ fontWeight: 'inherit', flexGrow: 1 }}>
+            <Box component={LabelIcon} color='var(--black-color)' sx={{ mr: 1 }} />
+            <Typography variant='body2' sx={{ fontWeight: 'inherit', flexGrow: 1, fontFamily: 'inherit' }}>
               {labelText}
             </Typography>
-            <Typography variant='caption' color={isFull ? 'error' : 'inherit'}>
+            <Typography
+              variant='caption'
+              color={isFull ? 'error' : 'inherit'}
+              sx={{ fontFamily: 'inherit', fontWeight: isFull ? 600 : 'inherit' }}
+            >
               {labelInfo}
             </Typography>
           </Box>

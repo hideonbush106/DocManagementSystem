de6be1741508f29e5f915bdd03d4bf41c1830491
DaiNghia0212 <dainghia12311@gmail.com>
2023-06-28 15:54:09 +0700

    Main Layout

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,13 +1,18 @@
+// react
 import React from 'react'
-import BasicLayout from './BasicLayout'
-import { useTheme } from '@mui/material/styles'
-import useMediaQuery from '@mui/material/useMediaQuery'
+// MUI
+import { Grid, Typography } from '@mui/material'
 import AddRoundedIcon from '@mui/icons-material/AddRounded'
 import KeyboardReturnRoundedIcon from '@mui/icons-material/KeyboardReturnRounded'
+import { useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+// coponents
+import BasicLayout from './BasicLayout'
 import { ImportButton, ReturnButton } from '../button/Button.tsx'
 import ImportDocumentModal from '../modal/ImportDocumentModal'
-import SpeedDialCustom from '../SpeedDial/SpeedDial.tsx'
-import { Grid } from '@mui/material'
+import SpeedDialCustom from '../speed-dial/SpeedDial.tsx'
+// styles
+import { IconDiv } from './MainLayout.styled.ts'
 
 type Props = {
   children: React.ReactNode
@@ -18,6 +23,7 @@ const MainLayout = (props: Props) => {
   const [speedDialOpen, setSpeedDialOpen] = React.useState(false)
   const [importDocumentModalOpen, setImportDocumentModalOpen] = React.useState(false)
   const theme = useTheme()
+  const belowLg = useMediaQuery(theme.breakpoints.down('lg'))
   const belowMd = useMediaQuery(theme.breakpoints.down('md'))
   const { children, title } = props
 
@@ -28,6 +34,27 @@ const MainLayout = (props: Props) => {
   const handleImportDocumentModalClose = () => {
     setImportDocumentModalOpen(false)
   }
+
+  const items = [
+    {
+      icon: <img src='/assets/department.svg' alt='department' />,
+      iconBackground: 'blue',
+      title: 'Departments',
+      content: '5 Departments'
+    },
+    {
+      icon: <img src='/assets/user.svg' alt='member' />,
+      iconBackground: 'purple',
+      title: 'Members',
+      content: '5 Members'
+    },
+    {
+      icon: <img src='/assets/folder.svg' alt='document' />,
+      iconBackground: 'green',
+      title: 'Documents',
+      content: '10000 Files'
+    }
+  ]
 
   const actions = [
     {
@@ -58,22 +85,64 @@ const MainLayout = (props: Props) => {
     <BasicLayout title={title}>
       {/* render when screen above medium */}
       {!belowMd && (
-        <Grid container spacing={2} justifyContent='space-between' mb='10px'>
-          <Grid item xs={6}>
-            <p>data</p>
+        <Grid container alignItems='center' justifyContent='space-between' marginY='10px' paddingX='8px'>
+          <Grid item container md={12} lg={7.45}>
+            {items.map((item, index) => (
+              <>
+                <Grid
+                  item
+                  container
+                  md={3.9}
+                  lg={3.7}
+                  key={index}
+                  alignItems='center'
+                  justifyContent={belowLg ? 'center' : 'flex-start'}
+                >
+                  <Grid item mr='8px'>
+                    {item.iconBackground === 'blue' ? (
+                      <IconDiv blue>{item.icon}</IconDiv>
+                    ) : item.iconBackground === 'purple' ? (
+                      <IconDiv purple>{item.icon}</IconDiv>
+                    ) : (
+                      <IconDiv green>{item.icon}</IconDiv>
+                    )}
+                  </Grid>
+                  <Grid
+                    item
+                    container
+                    direction='column'
+                    lg
+                    justifyContent='space-between'
+                    alignItems={belowLg ? 'center' : 'start'}
+                  >
+                    <Typography fontFamily='inherit' fontSize='15px' color='var(--gray-color)'>
+                      {item.title}
+                    </Typography>
+                    <Typography fontFamily='inherit' fontSize='15px' color='var(--black-color)'>
+                      {item.content}
+                    </Typography>
+                  </Grid>
+                </Grid>
+                {index <= items.length - 2 && (
+                  <Grid md={0.1} lg={0.3} style={{ borderLeft: '1px solid var(--gray-color)' }}></Grid>
+                )}
+              </>
+            ))}
           </Grid>
-          <Grid item container xs={5} spacing={2} justifyContent='flex-end'>
-            <Grid item>
-              <ImportButton text='New Document' />
+          {!belowLg && (
+            <Grid item container lg={4.55} spacing={1} justifyContent='flex-end'>
+              <Grid item>
+                <ImportButton text='New Document' />
+              </Grid>
+              <Grid item>
+                <ReturnButton text='Return Document' />
+              </Grid>
             </Grid>
-            <Grid item>
-              <ReturnButton text='Return Document' />
-            </Grid>
-          </Grid>
+          )}
         </Grid>
       )}
-      {/* render when screen below medium */}
-      <SpeedDialCustom actions={actions} open={speedDialOpen} onClick={handleSpeedDial} hidden={!belowMd} />
+      {/* render when screen below large */}
+      <SpeedDialCustom actions={actions} open={speedDialOpen} onClick={handleSpeedDial} hidden={!belowLg} />
       {/* Modals */}
       <ImportDocumentModal open={importDocumentModalOpen} handleClose={handleImportDocumentModalClose} />
       {children}

e4379bd7a6fcb344bc5a31b9149d3c0d09e50550
ddwcsanh <ledoducanh03@gmail.com>
2023-06-30 11:24:23 +0700

    Locker Advanced

--- src/pages/advanced/locker/LockerAdvanced.tsx
+++ src/pages/advanced/locker/LockerAdvanced.tsx
@@ -1,20 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Apartment, ExpandLess, ExpandMore, MeetingRoom, ViewModule } from '@mui/icons-material'
+import { ViewModule } from '@mui/icons-material'
 import AddRoundedIcon from '@mui/icons-material/AddRounded'
 import {
+  Autocomplete,
   Box,
   CircularProgress,
-  Collapse,
-  Divider,
   List,
   ListItemButton,
   ListItemIcon,
-  ListItemText
+  ListItemText,
+  TextField
 } from '@mui/material'
 import React, { useEffect, useState } from 'react'
-import { DeleteButton, UpdateRoomButton } from '~/components/button/Button'
-import CreateRoomModal from '~/components/modal/advanced/room/CreateRoom'
-import { CreateRoom, Department, Room, UpdateRoom } from '~/global/interface'
+import { DeleteButton } from '~/components/button/advanced/DeleteButton'
+import { UpdateButton } from '~/components/button/advanced/UpdateButton'
+// import { DeleteButton, UpdateRoomButton } from '~/components/button/Button'
+import CreateAdvancedModal from '~/components/modal/advanced/CreateAdvancedModal'
+import { CreateLocker, Department, Room, UpdateLocker } from '~/global/interface'
 import { notifyError, notifySuccess } from '~/global/toastify'
 import useDepartmentApi from '~/hooks/api/useDepartmentApi'
 import useLockerApi from '~/hooks/api/useLockerApi'
@@ -29,66 +31,68 @@ const LockerAdvanced = () => {
   const [selectedRoom, setSelectedRoom] = useState<Room>({ id: '', name: '', capacity: 0 })
   const { getAllDepartments } = useDepartmentApi()
   const { getRoomsInDepartment } = useRoomApi()
-  const { getLockerInRoom } = useLockerApi()
+  const { getLockerInRoom, createLocker, deleteLocker, updateLocker } = useLockerApi()
   const [loading, setLoading] = React.useState<boolean>(true)
   const [loadingRoom, setLoadingRoom] = React.useState<boolean>(true)
   const [loadingLocker, setLoadingLocker] = React.useState<boolean>(false)
-  const [isModalopen, setModalopen] = useState(false)
+  const [isModalOpen, setModalOpen] = useState(false)
 
-  const [openDept, setDeptOpen] = React.useState(false)
-  const [openRoom, setRoomOpen] = React.useState(false)
-
-  //handle options dropdown
-  const handleDeptOptions = () => {
-    setDeptOpen(!openDept)
+  //option for autocomplete
+  const optionsDept = {
+    options: departments.map((option) => option.name)
   }
 
-  const handleRoomOptions = () => {
-    setRoomOpen(!openRoom)
+  const optionsRoom = {
+    options: rooms.map((option) => option.name)
   }
+
   //handle modal open
-  const handleModalopen = () => {
-    setModalopen(true)
+  const handleModalOpen = () => {
+    setModalOpen(true)
   }
 
-  //handle dropdown close after selecting
-  const handleSelectDept = (dept: Department) => {
-    setSelectedDepartment(dept)
-    setLoadingRoom(true)
-    setDeptOpen(!openDept)
+  const handleDeptChange = (_event: React.SyntheticEvent<Element, Event>, value: string | null) => {
+    if (value !== null) {
+      const selectedDept = departments.find((dept) => dept.name === value)
+      if (selectedDept) {
+        setSelectedDepartment(selectedDept)
+        setLoadingRoom(true)
+      }
+    }
   }
 
-  const handleSelectRoom = (room: Room) => {
-    setSelectedRoom(room)
-    setLoadingLocker(true)
-    setRoomOpen(!openRoom)
+  const handleRoomChange = (_event: React.SyntheticEvent<Element, Event>, value: string | null) => {
+    if (value !== null) {
+      const selectedRoom = rooms.find((room) => room.name === value)
+      if (selectedRoom) {
+        setSelectedRoom(selectedRoom)
+        setLoadingLocker(true)
+      }
+    }
   }
 
   const fetchDepartment = async () => {
-    await getAllDepartments().then((result) => {
-      setDepartments(result.data)
-      setSelectedDepartment(result.data[0])
-    })
+    const result = await getAllDepartments()
+    setDepartments(result.data)
+    setSelectedDepartment(result.data[0])
   }
 
   const fetchRooms = async () => {
     if (selectedDepartment.id) {
-      await getRoomsInDepartment(selectedDepartment.id).then((result) => {
-        setRooms(result.data)
-        setSelectedRoom(result.data[0])
-      })
+      const result = await getRoomsInDepartment(selectedDepartment.id)
+      setRooms(result.data)
+      setSelectedRoom(result.data[0])
     }
   }
 
   const fetchLockers = async () => {
     if (selectedRoom !== undefined) {
       if (selectedRoom.id) {
-        await getLockerInRoom(selectedRoom.id).then((result) => {
-          setLockers(result.data)
-          setLoading(false)
-          setLoadingRoom(false)
-          setLoadingLocker(false)
-        })
+        const result = await getLockerInRoom(selectedRoom.id)
+        setLockers(result.data)
+        setLoading(false)
+        setLoadingRoom(false)
+        setLoadingLocker(false)
       }
     } else {
       notifyError('Create a room first')
@@ -107,181 +111,100 @@ const LockerAdvanced = () => {
     fetchLockers()
   }, [selectedRoom])
 
-  // const handleCreate = async (values: CreateRoom) => {
-  //   try {
-  //     await createRoom(values).then((result) => {
-  //       if (result) {
-  //         setLoadingLocker(true)
-  //         setRooms([]) // Clear the room array
-  //         notifySuccess('Create successfully')
-  //         setModalopen(false)
-  //       }
-  //     })
-  //     await fetchRooms() // Fetch the updated data
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
+  const handleCreate = async (values: CreateLocker) => {
+    values.room.id = selectedRoom.id
+    const result = await createLocker(values)
+    if (result) {
+      setLoadingLocker(true)
+      setLockers([]) // Clear the locker array
+      notifySuccess('Create successfully')
+      setModalOpen(false)
+    }
+    await fetchLockers() // Fetch the updated data
+  }
 
-  // const handleDelete = async (id: string) => {
-  //   try {
-  //     await deleteRoom(id).then((result) => {
-  //       if (result) {
-  //         setLoadingLocker(true)
-  //         setRooms([]) // Clear the room array
-  //         notifySuccess('Delete successfully')
-  //         // setModalopen(false)
-  //       } else {
-  //         setLoadingLocker(true)
-  //       }
-  //     })
-  //     await fetchRooms() // Fetch the updated data
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
+  const handleDelete = async (id: string) => {
+    const result = await deleteLocker(id)
+    if (result) {
+      setLoadingLocker(true)
+      setLockers([]) // Clear the room array
+      notifySuccess('Delete successfully')
+    } else {
+      setLoadingLocker(true)
+    }
+    await fetchLockers() // Fetch the updated data
+  }
 
-  // const handleUpdate = async (values: UpdateRoom) => {
-  //   try {
-  //     await updateRoom(values).then((result) => {
-  //       if (result) {
-  //         setLoadingLocker(true)
-  //         setRooms([]) // Clear the room array
-  //         notifySuccess('Update successfully')
-  //       }
-  //     })
-  //     await fetchRooms() // Fetch the updated data
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
+  const handleUpdate = async (values: UpdateLocker) => {
+    const result = await updateLocker(values)
+    if (result) {
+      setLoadingLocker(true)
+      setLockers([])
+      notifySuccess('Update successfully')
+    } else {
+      setLoadingLocker(true)
+    }
+    await fetchLockers()
+  }
 
   return (
     <>
       {selectedDepartment.id && selectedRoom && (
-        <CreateRoomModal
-          open={isModalopen}
-          handleClose={() => setModalopen(false)}
-          deptId={selectedDepartment.id}
-          // onSubmit={handleCreate}
+        <CreateAdvancedModal<CreateLocker>
+          open={isModalOpen}
+          type='Locker'
+          handleClose={() => setModalOpen(false)}
+          onSubmit={handleCreate}
+          initialValues={{ name: '', capacity: 10, room: { id: selectedRoom.id } }}
+          max={100}
         />
       )}
       <List
         sx={{
           width: '100%',
-          height: { xs: 'calc(100vh - 92px - 6rem)', md: 'calc(100vh - 42px - 6rem)' },
+          height: { xs: 'calc(100vh - 210px)', md: 'calc(100vh - 160px)' },
+          borderRadius: '5px',
+          borderTopLeftRadius: '0px',
           bgcolor: 'var(--white-color)',
           padding: '1rem 0',
-          overflowY: 'scroll'
+          overflowY: 'auto'
         }}
         component='div'
       >
         {!loading ? (
           <>
-            <ListItemButton
-              onClick={handleDeptOptions}
-              sx={{
-                padding: { sm: '0 5rem', xs: '0 1rem' },
-                height: '52.5px'
-              }}
-            >
-              {openDept ? null : (
-                <ListItemIcon sx={{ color: 'var(--black-color)', minWidth: { sm: '56px', xs: '40px' } }}>
-                  <Apartment />
-                </ListItemIcon>
+            <Autocomplete
+              {...optionsDept}
+              id='department option'
+              size='medium'
+              autoComplete
+              value={selectedDepartment.name}
+              onChange={handleDeptChange}
+              sx={{ width: '100%', padding: { sm: '0 4rem', xs: '0 1rem' }, my: '1rem' }}
+              renderInput={(params) => (
+                <TextField label='Department' {...params} placeholder='Select department' variant='standard' />
               )}
-
-              <ListItemText
-                sx={{ paddingLeft: openDept ? { sm: '56px' } : '0' }}
-                primary={openDept ? 'Select department' : selectedDepartment.name}
-                primaryTypographyProps={{ fontFamily: 'inherit', color: 'var(--black-color)' }}
-              />
-              {openDept ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-            <Divider sx={{ margin: { sm: '0 4rem', xs: '0 1rem' } }} />
-            <Collapse in={openDept} timeout='auto' unmountOnExit>
-              <List component='div' disablePadding>
-                {departments.map((dept) => (
-                  <React.Fragment key={dept.id}>
-                    <ListItemButton
-                      sx={{
-                        padding: { sm: '0 5rem', xs: '0 1rem' },
-                        height: '52.5px'
-                      }}
-                      onClick={dept.id === selectedDepartment.id ? undefined : () => handleSelectDept(dept)}
-                      disableTouchRipple={dept.id === selectedDepartment.id}
-                      selected={dept.id === selectedDepartment.id}
-                    >
-                      <ListItemIcon sx={{ color: 'var(--black-color)', minWidth: { sm: '56px', xs: '40px' } }}>
-                        <Apartment />
-                      </ListItemIcon>
-                      <ListItemText
-                        primary={dept.name}
-                        primaryTypographyProps={{ fontFamily: 'inherit', color: 'var(--black-color)' }}
-                      />
-                    </ListItemButton>
-                    <Divider sx={{ margin: { sm: '0 4rem', xs: '0 1rem' } }} />
-                  </React.Fragment>
-                ))}
-              </List>
-            </Collapse>
+            />
             {!loadingRoom ? (
               <>
-                <ListItemButton
-                  onClick={handleRoomOptions}
-                  sx={{
-                    padding: { sm: '0 5rem', xs: '0 1rem' },
-                    paddingLeft: { sm: '8rem' },
-                    height: '52.5px'
-                  }}
-                >
-                  {openRoom ? null : (
-                    <ListItemIcon sx={{ color: 'var(--black-color)', minWidth: { sm: '56px', xs: '40px' } }}>
-                      <MeetingRoom />
-                    </ListItemIcon>
+                <Autocomplete
+                  {...optionsRoom}
+                  id='room option'
+                  size='medium'
+                  autoComplete
+                  value={selectedRoom.name}
+                  onChange={handleRoomChange}
+                  sx={{ width: '100%', padding: { sm: '0 4rem', xs: '0 1rem' }, my: '1rem' }}
+                  renderInput={(params) => (
+                    <TextField label='Room' {...params} placeholder='Select room' variant='standard' />
                   )}
-
-                  <ListItemText
-                    sx={{ paddingLeft: openRoom ? { sm: '56px' } : '0' }}
-                    primary={openRoom ? 'Select room' : selectedRoom.name}
-                    primaryTypographyProps={{ fontFamily: 'inherit', color: 'var(--black-color)' }}
-                  />
-                  {openRoom ? <ExpandLess /> : <ExpandMore />}
-                </ListItemButton>
-                <Divider sx={{ margin: { sm: '0 4rem', xs: '0 1rem' }, marginLeft: { sm: '7rem' } }} />
-                <Collapse in={openRoom} timeout='auto' unmountOnExit>
-                  <List component='div' disablePadding>
-                    {rooms.map((room) => (
-                      <React.Fragment key={room.id}>
-                        <ListItemButton
-                          sx={{
-                            padding: { sm: '0 5rem', xs: '0 1rem' },
-                            paddingLeft: { sm: '8rem' },
-                            height: '52.5px'
-                          }}
-                          onClick={room.id === selectedRoom.id ? undefined : () => handleSelectRoom(room)}
-                          disableTouchRipple={room.id === selectedRoom.id}
-                          selected={room.id === selectedRoom.id}
-                        >
-                          <ListItemIcon sx={{ color: 'var(--black-color)', minWidth: { sm: '56px', xs: '40px' } }}>
-                            <MeetingRoom />
-                          </ListItemIcon>
-                          <ListItemText
-                            primary={room.name}
-                            primaryTypographyProps={{ fontFamily: 'inherit', color: 'var(--black-color)' }}
-                          />
-                        </ListItemButton>
-                        <Divider sx={{ margin: { sm: '0 4rem', xs: '0 1rem' }, marginLeft: { sm: '7rem' } }} />
-                      </React.Fragment>
-                    ))}
-                  </List>
-                </Collapse>
+                />
                 {!loadingLocker ? (
                   <>
                     {lockers.map((locker) => (
                       <ListItemButton
                         key={locker.id}
-                        sx={{ paddingLeft: { sm: '11rem', xs: '1rem' }, paddingRight: { sm: '5rem', xs: '1rem' } }}
+                        sx={{ paddingLeft: { sm: '8rem', xs: '1rem' }, paddingRight: { sm: '5rem', xs: '1rem' } }}
                         disableTouchRipple
                       >
                         <ListItemIcon sx={{ color: 'var(--black-color)', minWidth: { sm: '56px', xs: '40px' } }}>
@@ -291,7 +214,7 @@ const LockerAdvanced = () => {
                           primary={locker.name}
                           primaryTypographyProps={{ fontFamily: 'inherit', color: 'var(--black-color)' }}
                         />
-                        <UpdateRoomButton
+                        {/* <UpdateRoomButton
                           text='Update'
                           id={locker.id}
                           name={locker.name}
@@ -303,16 +226,27 @@ const LockerAdvanced = () => {
                           id={locker.id}
                           handleDelete={() => console.log('Delete')}
                           type='locker'
+                        /> */}
+                        <UpdateButton<UpdateLocker>
+                          type='Locker'
+                          onSubmit={handleUpdate}
+                          initialValues={{
+                            id: locker.id,
+                            name: locker.name,
+                            capacity: locker.capacity
+                          }}
+                          max={100}
                         />
+                        <DeleteButton id={locker.id} name={locker.name} type='locker' handleDelete={handleDelete} />
                       </ListItemButton>
                     ))}
                     <ListItemButton
                       sx={{
-                        paddingLeft: { sm: '11rem', xs: '1rem' },
+                        paddingLeft: { sm: '8rem', xs: '1rem' },
                         paddingRight: { sm: '5rem', xs: '1rem' },
                         height: '53px'
                       }}
-                      onClick={handleModalopen}
+                      onClick={handleModalOpen}
                     >
                       <ListItemIcon sx={{ color: 'var(--black-color)' }}>
                         <AddRoundedIcon />

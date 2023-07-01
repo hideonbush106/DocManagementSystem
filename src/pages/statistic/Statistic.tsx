61b5bf1c5d1560d1d0e2bc3a2942d0c5e0b5925e
DaiNghia0212 <dainghia12311@gmail.com>
2023-07-01 10:03:32 +0700

    Lending Analysis chart

--- src/pages/statistic/Statistic.tsx
+++ src/pages/statistic/Statistic.tsx
@@ -10,6 +10,13 @@ const Statistic = () => {
     { name: 'Admin', color: 'var(--yellow-color)', value: 30 }
   ]
 
+  const lendingData = [
+    { name: 'HR', color: 'var(--primary-color)', value: 8 },
+    { name: 'Acc', color: 'var(--green-color)', value: 7 },
+    { name: 'Sales', color: 'var(--red-color)', value: 20 },
+    { name: 'Admin', color: 'var(--yellow-color)', value: 5 }
+  ]
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} lg={8}></Grid>
@@ -22,7 +29,15 @@ const Statistic = () => {
           <AnalysisChart items={importData} />
         </Paper>
       </Grid>
-      <Grid item xs={12} md={6} lg={4}></Grid>
+      <Grid item xs={12} md={6} lg={4}>
+        <Paper sx={{ backgroundColor: 'var(--white-color)', boxShadow: 'none', height: '328px', padding: '12px 16px' }}>
+          <Typography fontSize='13px' color='#797979' fontWeight={600}>
+            LENDING ANALYSIS
+          </Typography>
+          <TitleUnderline />
+          <AnalysisChart items={lendingData} />
+        </Paper>
+      </Grid>
       <Grid item xs={12} lg={8}></Grid>
     </Grid>
   )

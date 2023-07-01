0ececf4a223443dead446cb873e72289140e2c86
DaiNghia0212 <dainghia12311@gmail.com>
2023-07-01 09:23:59 +0700

    Import Analysis chart

--- src/pages/statistic/Statistic.tsx
+++ src/pages/statistic/Statistic.tsx
@@ -1,5 +1,31 @@
+import { Grid, Paper, Typography } from '@mui/material'
+import AnalysisChart from '~/components/chart/AnalysisChart'
+import { TitleUnderline } from './Statistic.styled'
+
 const Statistic = () => {
-  return <div>Statistic</div>
+  const importData = [
+    { name: 'HR', color: 'var(--primary-color)', value: 60 },
+    { name: 'Acc', color: 'var(--green-color)', value: 40 },
+    { name: 'Sales', color: 'var(--red-color)', value: 214 },
+    { name: 'Admin', color: 'var(--yellow-color)', value: 30 }
+  ]
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} lg={8}></Grid>
+      <Grid item xs={12} md={6} lg={4}>
+        <Paper sx={{ backgroundColor: 'var(--white-color)', boxShadow: 'none', height: '413px', padding: '12px 16px' }}>
+          <Typography fontSize='13px' color='#797979' fontWeight={600}>
+            IMPORTS ANALYSIS
+          </Typography>
+          <TitleUnderline />
+          <AnalysisChart items={importData} />
+        </Paper>
+      </Grid>
+      <Grid item xs={12} md={6} lg={4}></Grid>
+      <Grid item xs={12} lg={8}></Grid>
+    </Grid>
+  )
 }
 
 export default Statistic

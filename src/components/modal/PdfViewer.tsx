1ea2b6e5f8b734e039b797e2f9386440c350d9de
Nguyen Hoai Phong <andyhoaiphong2003@gmail.com>
2023-07-03 23:04:02 +0700

    Add PDF viewer

--- src/components/modal/PdfViewer.tsx
+++ src/components/modal/PdfViewer.tsx
@@ -1,50 +1,75 @@
 import React from 'react'
-import { Box, Button, Modal } from '@mui/material'
+import { Box, Button, Modal, Typography } from '@mui/material'
 import { Viewer, Worker } from '@react-pdf-viewer/core'
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
 import '@react-pdf-viewer/default-layout/lib/styles/index.css'
-import styled from 'styled-components'
+import ModalLayout from './ModalLayout'
+import { zoomPlugin, ZoomInIcon, ZoomOutIcon } from '@react-pdf-viewer/zoom'
+import { scrollModePlugin } from '@react-pdf-viewer/scroll-mode'
+import { CreateNewFolderOutlined, FileOpen } from '@mui/icons-material'
 
-const PdfView = () => {
-  const PdfContainer = styled(Worker)`
-    width: 30%;
-  `
-  const [open, setOpen] = React.useState(false)
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
+interface PdfViewProps {
+  open: boolean
+  handleClose: () => void
+}
+
+const PdfView = (props: PdfViewProps) => {
+  const { open, handleClose } = props
   const defaultLayoutPluginInstance = defaultLayoutPlugin()
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: {
-      xs: '95vw',
-      md: '60vw',
-      lg: '40vw'
-    },
-    bgcolor: 'white',
-    boxShadow: 24,
-    maxHeight: {
-      xs: '100vh',
-      md: '80vh'
-    }
-  }
   return (
     <>
-      <Button onClick={handleOpen}>Open PDF</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby='modal-modal-title'
-        aria-describedby='modal-modal-description'
-      >
-        <Box>
-          <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js'>
-            <Viewer plugins={[defaultLayoutPluginInstance]} fileUrl='assets/progit.pdf' />
-          </Worker>
+      <ModalLayout open={open} handleClose={handleClose}>
+        <Box
+          display={'inline-flex'}
+          sx={{
+            p: {
+              xs: 0.75,
+              sm: 1.5
+            },
+            position: 'sticky',
+            top: 0,
+            background: 'white',
+            zIndex: 1,
+            width: '100%',
+            boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px'
+          }}
+        >
+          <FileOpen fontSize='large' sx={{ color: 'var(--black-color)', mx: 1 }} />
+          <Typography
+            sx={{
+              fontWeight: 600,
+              color: 'var(--black-color)',
+              fontSize: {
+                xs: '1.5rem',
+                sm: '2rem'
+              }
+            }}
+            variant='h4'
+          >
+            Document Name
+          </Typography>
         </Box>
-      </Modal>
+        <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.8.162/build/pdf.worker.min.js'>
+          <Viewer fileUrl='assets/PROCESS_MATERIAL.pdf' />
+        </Worker>
+        <Box
+          sx={{
+            p: 1.5,
+            position: 'sticky',
+            bottom: -1,
+            zIndex: 1,
+            background: 'white',
+            display: 'flex',
+            justifyContent: 'end',
+            width: '100%',
+            boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px'
+          }}
+        >
+          <Button sx={{ my: 1, mr: 1 }} variant='contained' color='primary' onClick={handleClose}>
+            Cancel
+          </Button>
+        </Box>
+      </ModalLayout>
     </>
   )
 }
--- src/pages/home/Test.tsx
+++ src/pages/home/Test.tsx
@@ -1,8 +1,18 @@
+import { Button } from '@mui/material'
 import '@react-pdf-viewer/core/lib/styles/index.css'
+import React from 'react'
 import PdfViewer from '~/components/modal/PdfViewer'
 
 const Test = () => {
-  return <PdfViewer />
+  const [open, setOpen] = React.useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+  return (
+    <>
+      <Button onClick={handleOpen}>Open</Button>
+      <PdfViewer open={open} handleClose={handleClose} />
+    </>
+  )
 }
 
 export default Test

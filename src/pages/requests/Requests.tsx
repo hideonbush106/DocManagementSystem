e2d335043c99200777926459412c37cda795faaa
ddwcsanh <ledoducanh03@gmail.com>
2023-07-12 10:29:52 +0700

    Fix nav tabs responsive

--- src/pages/requests/Requests.tsx
+++ src/pages/requests/Requests.tsx
@@ -1,11 +1,12 @@
 import ImportRequest from './importRequest/ImportRequest'
 import BorrowRequest from './borrowRequest/BorrowRequest'
 import NavTabs from '~/components/tab/NavTabs'
+import { Input, Outbox, Output } from '@mui/icons-material'
 
 const Requests = () => {
   const tabs = [
-    { label: 'Import Requests', component: <ImportRequest /> },
-    { label: 'Borrow Requests', component: <BorrowRequest /> }
+    { label: 'Import Requests', component: <ImportRequest />, icon: Input },
+    { label: 'Borrow Requests', component: <BorrowRequest />, icon: Outbox }
   ]
   return (
     <>
